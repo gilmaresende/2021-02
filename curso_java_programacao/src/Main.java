5f05cb94c57514bec0898e490ffcd2fff86c2cc6
Gilmar <gilmar74796@gmail.com>
2021-05-26 18:02:22 -0300

    Atividade aula 24

--- curso_java_programacao/src/Main.java
+++ curso_java_programacao/src/Main.java
@@ -1,10 +1,34 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int x = 32;
+		System.out.println(x);
+		double y = 10.35784;
+		System.out.println(y);
 		
-		System.out.print("Olá Mundo");
-
+		//impressão formatada -- duas casas
+		System.out.printf("%.2f\n",y);
+		
+		//impressão formatada -- 4 casas (formatação padrão do dispositivo)
+		System.out.printf("%.4f\n",y);
+		
+		//impressão formatada -- 4 casas (formatação padrão Especifica)
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f\n",y);
+				
+		System.out.println("Olá Mundo");
+		
+		System.out.println("Resultado = " + x + " metros.");
+		
+		System.out.printf("Resultado = %.2f metros.\n", y);
+		
+		String nome = "Maria";
+		int idade = 32;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha %.2f reais", nome, idade, renda);
+		
 	}
 
 }
