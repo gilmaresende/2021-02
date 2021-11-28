617a790840f268c67fe7f69e54f0d50b84b0f4f3
Gilmar <gilmar74796@gmail.com>
2021-11-28 19:31:07 -0300

    Service layer, component registration

--- SECAO25/src/main/java/com/condelar/cursoS25/resouces/UserResource.java
+++ SECAO25/src/main/java/com/condelar/cursoS25/resouces/UserResource.java
@@ -1,20 +1,34 @@
 package com.condelar.cursoS25.resouces;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.condelar.cursoS25.entities.User;
+import com.condelar.cursoS25.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1l, "Ana", "ana.lara@gmail.com", "999999", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.finfById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ SECAO25/src/main/java/com/condelar/cursoS25/services/UserService.java
@@ -0,0 +1,27 @@
+package com.condelar.cursoS25.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.condelar.cursoS25.entities.User;
+import com.condelar.cursoS25.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	
+	public User finfById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
