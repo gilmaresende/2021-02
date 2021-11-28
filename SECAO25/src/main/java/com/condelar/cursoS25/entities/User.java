2d7c240bc5b266fbebd1e4b1a805727d989f67ed
Gilmar <gilmar74796@gmail.com>
2021-11-28 19:44:53 -0300

    Order, Instant, ISO 8601 p.1

--- SECAO25/src/main/java/com/condelar/cursoS25/entities/User.java
+++ SECAO25/src/main/java/com/condelar/cursoS25/entities/User.java
@@ -1,14 +1,19 @@
 package com.condelar.cursoS25.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -24,6 +29,9 @@ public class User implements Serializable {
 	private String fone;
 
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<Order>();
 
 	public User() {
 
@@ -93,6 +101,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
