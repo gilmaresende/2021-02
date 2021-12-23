7f25945286ce57ee7234454fb9087746c4c17e46
Gilmar <gilmar74796@gmail.com>
2021-12-23 18:30:57 -0300

    Exception handling - delete

--- SECAO25/src/main/java/com/condelar/cursoS25/services/UserService.java
+++ SECAO25/src/main/java/com/condelar/cursoS25/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.condelar.cursoS25.entities.User;
 import com.condelar.cursoS25.repositories.UserRepository;
+import com.condelar.cursoS25.resouces.exceptions.DataBaseException;
+import com.condelar.cursoS25.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +25,7 @@ public class UserService {
 
 	public User finfById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -29,11 +33,18 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
-		User vo = repository.getById(id);
+		User vo = repository.findById(id).get();
 		updateData(vo, obj);
 		return repository.save(vo);
 	}
