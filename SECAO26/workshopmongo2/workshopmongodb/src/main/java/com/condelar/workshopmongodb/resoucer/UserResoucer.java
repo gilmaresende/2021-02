379d190c480199fbf6695cf018fa151f7ebf66a2
Gilmar <gilmar74796@gmail.com>
2022-01-06 00:07:15 -0300

    Conectando ao MongoDB com repository e service

--- SECAO26/workshopmongo2/workshopmongodb/src/main/java/com/condelar/workshopmongodb/resoucer/UserResoucer.java
+++ SECAO26/workshopmongo2/workshopmongodb/src/main/java/com/condelar/workshopmongodb/resoucer/UserResoucer.java
@@ -1,26 +1,26 @@
 package com.condelar.workshopmongodb.resoucer;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.condelar.workshopmongodb.domain.User;
+import com.condelar.workshopmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResoucer {
 
+	@Autowired
+	UserService service;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-
-		User maria = new User("1", "Maria Brow", "m.gmail");
-		User alex = new User("2", "Alex Green", "a.gmail");
-		return ResponseEntity.ok().body(Arrays.asList(maria, alex));
-
+		return ResponseEntity.ok().body(service.findAll());
 	}
 
 }
