e63d6d9139e7254744eb4a744d6b763b49546b38
Gilmar <gilmar74796@gmail.com>
2022-01-09 16:38:06 -0300

    Endpoint para retornar os posts de um usuário

--- SECAO26/workshopmongo2/workshopmongodb/src/main/java/com/condelar/workshopmongodb/resoucer/UserResoucer.java
+++ SECAO26/workshopmongo2/workshopmongodb/src/main/java/com/condelar/workshopmongodb/resoucer/UserResoucer.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.condelar.workshopmongodb.domain.Post;
 import com.condelar.workshopmongodb.domain.User;
 import com.condelar.workshopmongodb.dto.UserDTO;
 import com.condelar.workshopmongodb.services.UserService;
@@ -57,6 +58,12 @@ public class UserResoucer {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/post", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> finPost(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 
 }
