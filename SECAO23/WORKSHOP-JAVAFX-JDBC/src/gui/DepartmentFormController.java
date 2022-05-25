ebf833341981fd6ff64981677487629691902f59
Gilmar <gilmar74796@gmail.com>
2022-05-25 19:14:16 -0300

    Validation exception

--- SECAO23/WORKSHOP-JAVAFX-JDBC/src/gui/DepartmentFormController.java
+++ SECAO23/WORKSHOP-JAVAFX-JDBC/src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -64,7 +67,9 @@ public class DepartmentFormController implements Initializable {
 			this.service.saveOrUpdate(entity);
 			nofifyDataChangeListener();
 			Utils.currentStage(e).close();
-		} catch (DbException ex) {
+		} catch (ValidationException ex) {
+			setErrosMessages(ex.getErros());
+		}catch (DbException ex) {
 			Alerts.showAlert("Error saving obkect", null, ex.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -77,8 +82,17 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	private Department getFormData() {
+		ValidationException exception = new ValidationException("Validation Error");
+		
 		Department obj = new Department();
 		obj.setId(Utils.tryParseToInt(txtId.getId()));
+		if(txtName.getText()== null || txtName.getText().trim().equals("")) {
+			exception.addErro("name", "Fild can1t be empty.");
+		}
+		
+		if(exception.getErros().size()>0) {
+			throw exception;
+		}
 		obj.setName(txtName.getText());
 		return obj;
 	}
@@ -105,5 +119,11 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
-
+	
+	private void setErrosMessages(Map<String, String> erros) {
+		Set<String> fiels = erros.keySet();
+		if(fiels.contains("name")) {
+			txtError.setText(erros.get("name"));
+		}
+	}
 }
